cfc4a2c8b3159c699f3e0cb7cb19df7a2ad62b08
ArmanAghania <Arman94.vergil94@gmail.com>
2024-02-01 21:07:00 +0330

    first commit

--- Image Processing/Session 9/Assignment/materwelon/materwelon.py
+++ Image Processing/Session 9/Assignment/materwelon/materwelon.py
@@ -4,13 +4,13 @@ import numpy as np
 
 def convert_to_materwelon(image_path):
     image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     if image is None:
         print("Error: Image cannot be loaded.")
         return
 
-    red_channel = image[:, :, 0].copy()
+    red_channel = image[:, :, 2].copy()
     green_channel = image[:, :, 1].copy()
 
     image[:, :, 1] = red_channel
